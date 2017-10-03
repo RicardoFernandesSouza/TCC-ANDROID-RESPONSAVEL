1773e583ce39c3c93e614d80307f6256afa8f91d
RicardoFernandesSouza <ricardo_fernandes.souza@hotmail.com>
2017-10-03 20:05:00 -0300

    Commit de preucação

--- app/src/main/java/com/example/ricardofernandes/tohomeresponsavel/TiraFoto.java
+++ app/src/main/java/com/example/ricardofernandes/tohomeresponsavel/TiraFoto.java
@@ -1,11 +1,16 @@
 package com.example.ricardofernandes.tohomeresponsavel;
-import android.app.Activity;
+
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,79 +18,165 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class TiraFoto extends Activity {
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
-    private File f;
-    public File getAlbumDir()
-    {
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
-        File storageDir = new File(
-                Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_PICTURES
-                ),
-                "BAC/"
-        );
-        // Create directories if needed
-        if (!storageDir.exists()) {
-            storageDir.mkdirs();
-        }
+@RuntimePermissions
+public class TiraFoto extends AppCompatActivity implements View.OnClickListener {
 
-        return storageDir;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+    Button btnTakePhoto;
+    ImageView ivPreview;
+
+    String mCurrentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar);
+        setContentView(R.layout.activity_tira_foto);
+        initInstances();
+        startCamera();
     }
-    private File createImageFile() throws IOException {
-        // Create an image file name
 
-        String imageFileName =getAlbumDir().toString() +"/image.jpg";
-        File image = new File(imageFileName);
-        return image;
+    private void initInstances() {
+        btnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
+        ivPreview = (ImageView) findViewById(R.id.ivPreview);
+//
+        btnTakePhoto.setOnClickListener(this);
+
+
+    }
+
+    /////////////////////
+    // OnClickListener //
+    /////////////////////
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnTakePhoto) {
+            TiraFotoPermissionsDispatcher.startCameraWithCheck(this);
+            startCamera();
+        }
+    }
+
+    ////////////
+    // Camera //
+    ////////////
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void startCamera() {
+        try {
+            dispatchTakePictureIntent();
+        } catch (IOException e) {
+        }
+    }
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("Access to External Storage is required")
+                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.cancel();
+
+                    }
+                })
+                .show();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tira_foto);
-        this.imageView = (ImageView)this.findViewById(R.id.imageView);
-        Button photoButton = (Button) this.findViewById(R.id.button);
-        photoButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                try {
-                    f = createImageFile();
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-
-
-            }
-        });
-    }
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-
-            Bitmap photo = BitmapFactory.decodeFile(f.getAbsolutePath());
-//            imageView.setImageBitmap(photo);
-
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
             Intent i = new Intent(Intent.ACTION_SEND);
             i.setType("message/rfc822");
-            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"raul.pop90@gmail.com"});
-            i.putExtra(Intent.EXTRA_SUBJECT, "Prima poza");
-            i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"ricardo_fernandes.souza@hotmail.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "Etapa x");
+            i.putExtra(Intent.EXTRA_TEXT   , "foto da etapa x");
 
-            Uri uri = Uri.fromFile(f);
-            i.putExtra(Intent.EXTRA_STREAM, uri);
+            Uri imageUri = Uri.parse(mCurrentPhotoPath);
+            i.putExtra(Intent.EXTRA_STREAM, imageUri);
             try {
                 startActivity(Intent.createChooser(i, "Send mail..."));
+                finish();
             } catch (android.content.ActivityNotFoundException ex) {
                 Toast.makeText(TiraFoto.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                finish();
+
+            }
+            // Show the thumbnail on ImageView
+            File file = new File(imageUri.getPath());
+//            try {
+//               InputStream ims = new FileInputStream(file);
+//              //  ivPreview.setImageBitmap( BitmapFactory.decodeStream(ims));
+//            } catch (FileNotFoundException e) {
+//               return;
+//            }
+
+            // ScanFile so it will be appeared on Gallery
+            MediaScannerConnection.scanFile(TiraFoto.this,
+                    new String[]{imageUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        TiraFotoPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Camera");
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                return;
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile( TiraFoto.this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        createImageFile());
+                takePictureIntent.putExtra( MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
